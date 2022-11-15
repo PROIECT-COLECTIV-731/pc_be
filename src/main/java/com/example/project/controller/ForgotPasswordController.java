dad5995183d9991490833712f5323f9b8ce99683
FreddiesPizza <gabrielserengheu@gmail.com>
2022-11-15 11:25:54 +0200

    Working on controller. Basic structure defined.

--- src/main/java/com/example/project/controller/ForgotPasswordController.java
+++ src/main/java/com/example/project/controller/ForgotPasswordController.java
@@ -1,4 +1,18 @@
 package com.example.project.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@ResponseBody
+@RequestMapping("/forgotPassword")
 public class ForgotPasswordController {
+
+    private UserRepository repository;
+
+    @GetMapping("/email")
+    public User getPasswordByEmail(@PathVariable string email){
+        return repository.findById(id);
+    }
+
 }
