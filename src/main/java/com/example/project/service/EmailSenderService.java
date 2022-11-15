bd25047a6f4349e61f653cb367181763b91ea10d
FreddiesPizza <gabrielserengheu@gmail.com>
2022-11-15 10:46:46 +0200

    Initial commit. Finished send email service

---
+++ src/main/java/com/example/project/service/EmailSenderService.java
@@ -0,0 +1,27 @@
+package com.example.project.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailSenderService {
+
+    private JavaMailSender mailSender;
+
+    public void sendEmail(String toEmail,
+                          String subject,
+                          String body) {
+        SimpleMailMessage message=new SimpleMailMessage();
+        message.setFrom("gabrielserngheu@gmail.com");
+        message.setTo(toEmail);
+        message.setText(body);
+        message.setSubject(subject);
+
+        mailSender.send(message);
+
+        System.out.println("Mail Sent successfully...");
+    }
+
+}
