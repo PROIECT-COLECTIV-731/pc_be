7bb6eb160caa7ea9c83c96100720eae334a9e4c9
Matei Grosu <79897568+matei31@users.noreply.github.com>
2022-11-16 19:45:55 +0200

    fixed typo

--- src/main/java/com/example/project/service/LoginService.java
+++ src/main/java/com/example/project/service/LoginService.java
@@ -2,7 +2,6 @@ package com.example.project.service;
 
 import com.example.project.entity.UserEntity;
 import com.example.project.repository.UserRepository;
-import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
@@ -22,9 +21,9 @@ public class LoginService {
     public String login(String email, String password){
         try {
             UserEntity userEntity = findByEmailAndPassword(email, password);
-            if (userEntity != null && password.contains("@stud.ubbcluj.ro"))
+            if (userEntity != null && email.contains("@stud.ubbcluj.ro"))
                 return "userAccepted";
-            else if (userEntity != null && password.contains("@ubbcluj.ro")) {
+            else if (userEntity != null && email.contains("@ubbcluj.ro")) {
                 return "adminAcceoted";
             }
             return null;
